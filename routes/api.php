8fb2c64f2102b8b16ff259a8a6f92cd7ec2a76e0
Batraz2004 <batraz.tolasovv@gmail.com>
2025-10-23 21:50:38 +0300

    create base sanctum authorization and registration

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('registration', [RegistrationController::class, 'createUser']);
+Route::post('login', [LoginController::class, 'login']);
+
